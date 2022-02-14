804a8299dbf8dead289206ef33f2a1e339dc4bda
muhammadirfan92 <irfanabbas034@gmail.com>
2022-02-14 12:23:57 +0500

    Completed first API

---
+++ my_api_project/api/serializers.py
@@ -0,0 +1,10 @@
+# this file will convert intances of Model Item objects into the
+#data type that the response method can understand.
+
+from rest_framework import serializers
+from base_api.models import Item
+
+class ItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Item
+        fields = '__all__'  # list of fields that we want to serailize
---
+++ my_api_project/api/views.py
@@ -0,0 +1,20 @@
+from encodings import search_function
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from base_api.models import Item
+from .serializers import ItemSerializer
+
+
+@api_view(['Get'])
+def getData(request):
+    items = Item.objects.all()
+    serializer = ItemSerializer(items, many=True)  #many tells to serialize multiple items,
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def addItem(request):
+    serializer = ItemSerializer(data=request.data)
+    if serializer.is_valid(): # checking if the passed data is valid
+        serializer.save()
+    return Response(serializer.data)
